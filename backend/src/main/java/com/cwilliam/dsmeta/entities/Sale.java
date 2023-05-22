3cefbb6578f1c0c3757f16b1f63cb55f7867fa3e
cwilliam98 <cristianwillian174@gmail.com>
2023-05-21 22:04:33 -0300

    Database

---
+++ backend/src/main/java/com/cwilliam/dsmeta/entities/Sale.java
@@ -0,0 +1,30 @@
+package com.cwilliam.dsmeta.entities;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String sellerName;
+
+    private Integer visited;
+
+    private Integer deals;
+
+    private Double amount;
+
+    private LocalDate date;
+}
