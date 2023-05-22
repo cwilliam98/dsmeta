5e33ffc2f2b5e50ce0f8efb72daa72bb297c032f
cwilliam98 <cristianwillian174@gmail.com>
2023-05-21 22:27:52 -0300

    Date select

--- backend/src/main/java/com/cwilliam/dsmeta/services/SalesService.java
+++ backend/src/main/java/com/cwilliam/dsmeta/services/SalesService.java
@@ -3,8 +3,13 @@ package com.cwilliam.dsmeta.services;
 import com.cwilliam.dsmeta.entities.Sale;
 import com.cwilliam.dsmeta.repositories.SalesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 @Service
 public class SalesService {
@@ -12,7 +17,14 @@ public class SalesService {
     @Autowired
     private SalesRepository repository;
 
-    public List<Sale> findSales(){
-        return repository.findAll();
+    public Page<Sale> findSales(String minDate, String maxData, Pageable pageable) {
+
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+
+        LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        LocalDate max = maxData.equals("") ? today : LocalDate.parse(maxData);
+
+        return repository.findSales(min, max, pageable);
     }
 }
