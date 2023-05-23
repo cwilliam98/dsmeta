daf481ecda1312fe019e2f0b9b38b27cdc014196
Cristian Albrecht <cristianwillian174@gmail.com>
2023-05-22 22:35:33 -0300

    Update index.tsx

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -14,6 +14,7 @@ function NotificationButton( {saleId} : Props) {
     function handleClick(saleId: number){
             axios(`${BASE_URL}/sales/${saleId}/notification`)
             .then(response => {
+                console.log(response);
                 toast.info("SMS enviado com sucesso");
             })
     }
